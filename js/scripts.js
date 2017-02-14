2d6276f300b12966445fa39bde0f4bf20b3784d6
julian flood and rachel gibbons <rachel@>
2017-02-13 18:46:51 -0800

    add appointment page with css,js,index page

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+
+    var nameInput = $("input#name").val();
+    var dateInput = $("input#date").val();
+    var startTimeInput = $("input#startTime").val();
+    var endTimeInput = $("input#endTime").val();
+
+    $(".name").text(nameInput);
+    $(".date").text(dateInput);
+    $(".startTime").text(startTimeInput);
+    $(".endTime").text(endTimeInput);
+
+    $("#confirmation").show();
+
+    event.preventDefault();
+  });
+});
